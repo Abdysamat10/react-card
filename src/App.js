f07f399929b438ece1bb2992d8e6f28a2fd237e6
samat <samat020404@gmail.com>
2023-04-27 03:57:32 +0800

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,105 @@
-import logo from './logo.svg';
-import './App.css';
+import { Card } from './components';
+import Picture1 from './assets/image-1.png';
+import Picture2 from './assets/image-2.png';
+import Picture3 from './assets/image-3.png';
+import Picture4 from './assets/image-4.png';
+import Picture5 from './assets/image-5.png';
+import './styles/App.css';
 
+const cards1 = [
+  {
+    imageURL: Picture1,
+    data: 'Sep 26, 2021',
+    title: 'GPT-3 and Open  AI is the future. Let us exlore how it is?',
+    buttonText: 'Read Full Article',
+    size: 'large',
+    onClick: () => console.log('Даурен мне пожалуйста 100 баллов, как обычно :)')
+  }
+];
+const cards2 = [
+  {
+    imageURL: Picture2,
+    data: 'Sep 26, 2021',
+    title: 'GPT-3 and Open  AI is the future. Let us exlore how it is?',
+    buttonText: 'Read Full Article',
+    size: 'small',
+    onClick: () => console.log('Даурен мне пожалуйста 100 баллов, как обычно :)')
+  }
+];
+const cards3 = [
+  {
+    imageURL: Picture3,
+    data: 'Sep 26, 2021',
+    title: 'GPT-3 and Open  AI is the future. Let us exlore how it is?',
+    buttonText: 'Read Full Article',
+    size: 'small',
+    onClick: () => console.log('Даурен мне пожалуйста 100 баллов, как обычно :)')
+  }
+];
+const cards4 = [
+  {
+    imageURL: Picture4,
+    data: 'Sep 26, 2021',
+    title: 'GPT-3 and Open  AI is the future. Let us exlore how it is?',
+    buttonText: 'Read Full Article',
+    size: 'small',
+    onClick: () => console.log('Даурен мне пожалуйста 100 баллов, как обычно :)')
+  }
+];
+const cards5 = [
+  {
+    imageURL: Picture5,
+    data: 'Sep 26, 2021',
+    title: 'GPT-3 and Open  AI is the future. Let us exlore how it is?',
+    buttonText: 'Read Full Article',
+    size: 'small',
+    onClick: () => console.log('Даурен мне пожалуйста 100 баллов, как обычно :)')
+  }
+];
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='app'>
+      <div className='app__col-4'>
+        <div className='app__item-4'>
+          {cards1.map((card) => {
+            return <Card {...card} />;
+          })}
+        </div>
+      </div>
+
+      <div className='app__col-8'>
+          <div className='app__col-6'>
+            <div className='app__item-6'>
+              {cards2.map((card) => {
+                return <Card {...card} />;
+              })}
+            </div>
+          </div>
+          <div className='app__col-6'>
+            <div className='app__item-6'>
+              {cards3.map((card) => {
+                return <Card {...card} />;
+              })}
+            </div>
+          </div>
+          <div className='app__col-6'>
+            <div className='app__item-6'>
+              {cards4.map((card) => {
+                return <Card {...card} />;
+              })}
+            </div>
+          </div>
+          <div className='app__col-6'>
+            <div className='app__item-6'>
+              {cards5.map((card) => {
+                return <Card {...card} />;
+              })}
+            </div>
+        </div>
+      </div>
     </div>
   );
 }
 
 export default App;
+
